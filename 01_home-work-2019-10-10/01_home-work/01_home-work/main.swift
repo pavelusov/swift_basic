85b94125a71df0c86fe859d9847223812116369b
Pavel Usov <ipavelusov@gmail.com>
2019-10-12 17:04:41 +0500

    1 home work

---
+++ 01_home-work-2019-10-10/01_home-work/01_home-work/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  01_home-work
+//
+//  Created by Павел Усов on 11/10/2019.
+//  Copyright © 2019 Pavel Usov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
