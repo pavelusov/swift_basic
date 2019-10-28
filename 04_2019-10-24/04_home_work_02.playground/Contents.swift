c6e58ae6bfe84db60ff66d0887f2fde1711c0482
Pavel Usov <ipavelusov@gmail.com>
2019-10-28 21:38:36 +0500

    Home work 5.

---
+++ 04_2019-10-24/04_home_work_02.playground/Contents.swift
@@ -0,0 +1,58 @@
+// Напишите функцию для сложения любых двух числовых аргументов
+// + extra String, Set, Array
+import Foundation
+
+protocol Summable {
+    associatedtype Member
+    static func +(a: Self, b: Self) -> Self
+}
+
+extension Int: Summable {
+    typealias Member = Int
+}
+
+extension Float: Summable {
+    typealias Member = Float
+}
+
+extension Double: Summable {
+     typealias Member = Double
+}
+
+// Extra
+extension String: Summable {
+    typealias Member = String
+    static func +(lhs: Member, rhs: Member) -> Member {
+        return "\(lhs) \(rhs)"
+    }
+}
+
+extension Set: Summable {
+    typealias Member = Set
+    static func +(lhs: Member, rhs: Member) -> Member {
+        return lhs.union(rhs)
+    }
+}
+
+extension Array: Summable {
+    typealias Member = Element
+}
+
+func add<T: Summable>(_ a: T, _ b: T) -> T {
+    return a + b
+}
+
+let intSum = add(1, 2)
+
+let floatSum  = add(Float(1.0), Float(2.2))
+
+let doubleSum = add(1.22, 2.34)
+
+// Extra
+let stringSum = add("Hello", "World")
+
+let setSum = add(Set([1, 2, 3]), Set([2, 4, 7]))
+
+let intArraySum = add([1, 3], [22, 3, 5])
+
+let stringArraySum = add(["1", "A", "4"], ["22", "3", "4"])
